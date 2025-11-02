cbb22b10eb302f5a3c731cd5605b5ef2304e478b
Atharva Kadam <154217374+ak26009@users.noreply.github.com>
2025-11-02 11:22:25 +0530

    Create add.c

---
+++ fork() and exec()/add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    printf("Addition result = %d\n", a + b);
+    return 0;
+}
