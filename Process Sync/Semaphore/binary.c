7cc0d741e4d2405656270dfa07be2ba73446d8e8
Atharva Kadam <154217374+ak26009@users.noreply.github.com>
2025-11-07 23:26:35 +0530

    Create binary.c

---
+++ Process Sync/Semaphore/binary.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t bathroom; 
+
+void* person(void* name) {
+    char* person_name = (char*)name;
+
+    printf("%s is waiting to use the bathroom...\n", person_name);
+
+    sem_wait(&bathroom);
+    printf("%s entered the bathroom \n", person_name);
+
+    sleep(10); 
+
+    printf("%s is leaving the bathroom ️\n", person_name);
+    
+    sem_post(&bathroom); 
+    
+    return NULL;
+}
+
+int main() {
+    pthread_t p1, p2;
+    sem_init(&bathroom, 0, 1);
+
+    pthread_create(&p1, NULL, person, "Alice");
+    pthread_create(&p2, NULL, person, "Bob");
+
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+    
+    sem_destroy(&bathroom);
+    return 0;
+}
