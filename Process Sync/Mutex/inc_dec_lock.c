529103aee103e82c09f6126117795aefbf7ac74f
Atharva Kadam <154217374+ak26009@users.noreply.github.com>
2025-11-17 07:40:01 +0530

    Update inc_dec_lock.c

--- Process Sync/Mutex/inc_dec_lock.c
+++ Process Sync/Mutex/inc_dec_lock.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <pthread.h>
 
 int count = 0;
@@ -29,21 +29,14 @@ void *decthread(void *data) {
 int main() {
     pthread_t incid, decid;
     
-
     pthread_mutex_init(&lock, NULL);
     
-
     pthread_create(&incid, NULL, incthread, NULL);
-    
-
     pthread_create(&decid, NULL, decthread, NULL);
 
-   
     pthread_join(incid, NULL);
     pthread_join(decid, NULL);
 
-
     pthread_mutex_destroy(&lock);
-
     return 0;
 }
